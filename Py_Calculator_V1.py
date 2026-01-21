943573823dea92d1c8cc4980776386ddedc0db97
Dr.M Dev <ms.dental.business@gmail.com>
2026-01-21 16:49:33 +0300

    Basic py-calculator project_v1

---
+++ Py_Calculator_V1.py
@@ -0,0 +1,232 @@
+#Py-Calculator by Dr.M-Dev:
+
+def logo():
+    print('''       
+          
+                                                             !J!:                                                                
+                                                              ^G@@&P7:                                                           
+                                         .~7YGB#&&&&&&&#BG5?~:  .Y@@@@&G^                                                        
+                                    :?P&@@@@@@@@@@@@@@@@@@@@@@@&G?J@@@@@&                                                        
+                                .!G@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@P   ...                                                 
+                              ~B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@BG&&@@@@                                              
+                            ?&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&                                             
+                          7@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#GYP#&J                                            
+                        .B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!                                                
+                       :&@@@@@@@@@@@@@@@@J7@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@P                                               
+                      .@@@@@@@@@@@@@@@@#:  ^&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@G                                              
+                      #@@@@@@@@@@@@@@&7      P@@@@@@@@@@@@@B&@@@@@@@&@@#&@@@@@@@@@@J                                             
+                     !@@@@@@@@@@@@@&?         ^#@@@@&&@@@@@@#PPGB##? B@5#@#@@@@@@@@@:                                            
+                     B@@@@@@@@@@@G~             ^B@@@&GG#@@@@@@@#~   .&#J@Y&@@@@@@@@G                                            
+                     @@@@@@@@@@~                  .?#@@@&BGPGBBJ      .#5G&J@@@@@@@@@.                                           
+                   .@@@@@@@@@7      !PB##B4^        .^JG#&&P:  ^4B###P4?!~!?@@@@@@@@^                                           
+                   .@@@@@@@@#      !4~.. .~4^                 ~4~....~4^    #@@@@@@@^   .~                                      
+               ~BJ :@@@@@@@@BJYYYYYYJJJJYJJJJJJJ?!.     .!?JYYYJJYYYJJJYYYYY&@@@@@@@P7: .G#?.                                   
+            .?BG^  &@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@?...5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@?  .Y#5:                                 
+          :5BJ.    @@@@@@@@@&PJJJJJJ????JJJJ5&@@@@@@@@@@@@@@@GYJJ??????JJJJJYB@@@@@@@@@Y     7BB~                               
+        !GG~       .YGG@@@@B        ...       G@@@@@@@@@@@@@:     .::.        !@@@@&GP7        ^G#?                             
+     .JB5:             &@@@Y      4P..P@G.    7@@@@@@@@@@@@&    ^:^B@@@P.      @@@@~             .J#5.                          
+     J@J               &@@@5     G@@4Y&@@&    ?@@@@?::^#@@@&   ~@4^B@@@@#     .@@@@~              ^#&:                          
+      .5#J.            &@@@5     ?@@@@@@@P    7@@@@.   ?@@@&   .@@@@@@@@Y     .@@@@~           .J#P:                            
+        .?#P:          #@@@G      .JGBBY:     5@@@&    ^@@@@.    ?B&&#P^      :@@@@~         ^PBJ.                              
+           ~BB!        7@@@@5.              :P@@@@J     #@@@&!.             .!&@@@&        7BG~                                 
+             :5#J.      ?@@@@@@@@@@@@@@@@@@@@@@@@J      .#@@@@@@&&&&&&&@&&@@@@@@@B.     :5BY.                                   
+               .J#!      .7G&&@@@@@@@@@@@@@@&&G7.         ^5#&@@@@@@@@@@@@@@@&BJ:       7!     
+
+
+
+                        .^!7??JJYYY555555555555555YYYYJJJ??77!!~^^::..                       
+                   .?GPYJ?77!!!~~~~~~^^^^^~~~~~~!!!!77??JJJYY5555PP555YYJ?7!~^::.         
+                   5#7                                          ...:^^~!7?JJY5PPPP55J!:   
+                  ~&Y  !55Y5??YYYYJJJ??777!!~~^^^::....                         .:^!Y#BY: 
+                  ?&~ :#?. ....:::^^~~~!!77???JJJJYYYYYYYYYJJJJ??777!!~~^^^::...     ?##P.
+                  5#: !#:                              ...:::^^^~~!!!777???JJJJY5?.  ~###^
+                 .BG  YG          Py-Calculator                                 7&7  !##B:
+                 ^#Y  GJ       		     you found an easter-egg!  :)       !&!  ?##G.
+                 7&7 .B?                                      congrats <3       J#^  J##P 
+                 5#^  !5YYYJJJJ???777!!~~~^^:::....                             5B.  5##Y 
+                ^#P     ...::::^^~~~!!777?????JJJJJJJJJJJ????777!!~~~^^^:::....:BP  .G##7 
+                5#~  ....                           .....:::^^^~~~!!!7777??????JJ:  :B##~ 
+               !&Y  77~~~!!!!7!7~ .~~~~^^^^:::                                      ~##B: 
+              .BB: 7J  :!!!:   J&JY^.::^^^~~~75!:?!!!!!!!!!!!^  :^^^^:::...         ?##G. 
+              Y&7 :5.  1111   :BBG!   YJY!   !#G5^  .^~:....7#J?7^^^^~~~!!!7J^      P##Y  
+             ~#P  Y!   ~5:    5#GY   2222   .G#PJ   3333    ?BG5   .^ ..   ~#B.    ~##P^  
+            .G#^  P?!!~~^:::.7#GP~   ^!!!   J&GP:  .JJPY   !PGP~   ++++    5&5     Y##Y.  
+            ?&J   ~5PPPPGGGGGBB~?P5YYJJ?777?##JP:::.:^:   .G&P5    !~7?   ~##^    :B##J   
+           ^#G. :J~^~~~!77??J!..~?JYYY55PPP55! JPGGGPPPP55G&G!P?77!~^^^:::P&5     ?###^   
+           5#!  5^   :7~    5#?Y:.::^^^~~~7Y~.7!!77??JJJYY?!. !55PPPGGGGGGBG^    .G##5    
+          7&5  7J   4444   ~#BG^   JY?!   ~&G5~   .::..::!G?^J~^~~!!777???~      7###!    
+         :BB: :P.  ~!J5^  .G#PJ   5555    YPY?   6666   ?&GP^    .-    .P#~     P##G.    
+         Y&?  !G!~^^::... J&GP:   -d-    ~Y??:  .Y!YG.  :B#PJ   ----    ^##~    ~###?     
+        ~#P   .JPPPGGGGGPG#B~PYJ??7!!~^^!BBJY    :^!:   Y&GP:   .JJ~.   5&5     Y##B:     
+       .G#~  !7~!!!77??JJ?~..~JY55PPPGGGGG?:PPP55YJJ??7J##?G^:..       ~##~    :B##Y      
+       ?&Y  ~J    :!. ..~B5!?^^^^~~!777J7: ^!?JJYY555PP557 ?GGGGPPP55YYB&5     J###^      
+      ^BB:  Y:  .7777   J#G5.   !7?^   ?&YJ!.:::^^~~!!?Y^ !!77??JJJYYYYJ7.    .G##P       
+      5&7  77    ^B~   ~#BP~   .8888  .G#PJ   9999    !&GJ!  ...::^^~~YY:     7###!       
+     !&P   P^... .:   .G#PY   .JP57   Y#G5:   ::Y#^  .G#PY   XXXX     P&Y     P##G.       
+    .B#^   7PGPPPP55YJP&G?5^^:::.::  ~##P?    ??JJ.  ?#B5~   ~???.   !##~    ~###?        
+    J&J   !!77??JJYY5YJ?:.JPGGGGGPPP5B&5?5!!~^:::.. ^B#55           .G#5     5##B:        
+   ~#G.  J7   :^::^^~?Y:^7!!!7???JJYJ7! .?PPPGGGGGGPB#G^PJ?7!!~^^:..?##~    ^###Y         
+  .P#!  ~Y   JY?P.   Y&5Y. :^: :::^^JP:!?~!!777???JJ7~. ~Y55PPGGGGGG##Y     J###^         
+  ?&5  .5^  Rr2R     !#BP~ :5Y?~~    P&5Y       ..::!G?:J~~~!!777????!^     :B##P          
+ :BB:  7J   .777.  .G#PY   00000    7#GP~  CCCC    ?&G5^        ...?B7     !555~          
+ 5&?   7GYJ?7~~^^::Y&PP^   ::.??~ :B#PY    .?!    :B#5J   ÷÷÷÷     5&P     JYYJ           
+~#G.    ~?JY5PGGGBB#G^YPYYJ?7!~^::5&PP~     .     Y#GP^  :JJJY7   ~##!    !#B#?           
+7#B~           ..::^. .!?JY5PPGGBB#G^JG5YJJ?7!~^:!##Y5       .    P#P     P##B:           
+:G##5?!~^^::..                 ..::.  ~7?JY5PGGGBBBY:PP5YJ?7!!~^:7##!    ~###J            
+ :5B&&&####BBGPP5YYJ?7!~~^::..                 .:::  .!?JY55PGGBB##Y     5###^            
+   :!JY5PGBB###&&&&&&&####BBGPPP5YJ??7!~^^:..                 ..:::     J###5             
+         ..::^~!7?JY5PPGBB##P5&&&&&&&#####BBGGP55YJ?7!!~^::..        .!5###B^             
+                       ..:^^^~7?JY5PPGBBB####&&&&&&&#####BBBGGP55Y55PB####B!              
+                                     ..::^~!77?JY5PPGGBB################GJ:               
+                                                    ..:^^~!7?JYY5555YJ7~.       
+                                                                                                                                               
+                                                              ...::::.      ...::::::::    :.      .:.   
+  5@@@@@@@@B!    &@@@@@@@&G:        ^G&@@@&P#@@@@B~          J@@@@@@@@@G.   #@@@@@@@@@@   .@@B    7@@?   
+  G@@~::::J@@!   @@#     B@@.      :@@G::~&@@!::Y@@~         J@@~    ^@@B   #@@.           !@@J  .@@B    
+  G@@     .@@Y   @@@    5&@#       ~@@!   B@&   :@@?         J@@:     &@#   #@@BBBBBBB      P@@: #@@.    
+  7BP     .@@J   PBGGGGGB@@B       :BB^   B@&   :@@?         ~GP.     &@#   JGPYYYYYYY       &@# @@!     
+  Y&&^....?@@7   #&P     J@@:  ##  ^&&~   B@&   ^@@?         ?@@7:  :7@@P   Y@& ......       ^@@@@P      
+  P@@@@@@@@&?    &@B     ?@@:  ##  ~@@!   B@&   :@@?         ?@@@@@@@@#J    J&@@@@@@@@?       ?@@B  
+                                 
+''')
+
+    print("******** WELCOME TO Py-Calculator - By: Dr.m DEV *********")
+#================================================================================================
+#================================================================================================
+#functions/operations
+def add(n1, n2):
+    """ le-add stuff"""
+    return n1 + n2
+
+def sub(n1, n2):
+    """ le-minus stuff xD"""
+    return n1 - n2
+
+def divide(n1, n2):
+    """ divide people :("""
+    return n1 / n2
+
+def multiply(n1, n2):
+    """ bom bam beem baraBOOM! numbers"""
+    return n1 * n2
+
+
+#______________________________________________________________________________________________________
+def reset():
+    is_code_running = False
+    is_code_running = True
+    print("\n" * 1000) #Reset-Spacer :)
+    logo()
+
+#______________________________________________________________________________________________________
+addition = add
+subtraction = sub
+division = divide
+multiplication = multiply
+#---------------------------
+operations = {
+    "+" : addition,
+    "-" : subtraction,
+    "/" : division,
+    "*" : multiplication
+}
+
+
+
+#================================================================================================
+#================================================================================================
+# =================================================================
+is_code_running = True
+#XXXXXXXXXXXXXXXXXX
+run_code_1st = True
+#--------------------
+operation_1st_result = 0
+saved_result = 0
+#--------------------
+num1 = 0
+operation_choice = ""
+num2 = 0
+#--------------------
+allowed_numbers = []
+allowed_symbols = ["+", "-", "*", "/"]
+#--------------------
+
+
+
+#=======================================================================================================================
+#Code_Starts_Here :>
+logo()
+
+while is_code_running:
+    saved_result = 0
+    operation_1st_result = 0
+    run_code_1st = True
+
+    while run_code_1st == True and saved_result == 0:
+        num1 = float(input("what's the first number?"))
+        operation_choice = input("select a mathematical operation,\nfor addition [+]\nfor subtraction [-]\nfor multiplication [*]\nfor division [/]\nthe operation you chose is: ")
+        num2 = float(input("what's the next number?"))
+
+        #-------------------
+        if num1 in allowed_numbers == False or num2 in allowed_numbers == False:
+            print("<!>INVALID numbers")
+        if operation_choice in allowed_symbols == False:
+            print("<!>INVALID operation input, please only select from the following symbols [-][+][*][/]")
+
+        # ------------------
+        for operation_ver in allowed_symbols:
+            if operation_choice == operation_ver:
+                operation_1st_result = operations[operation_ver](num1,num2)
+
+        #------------------
+        print(f"result: {num1} {operation_choice} {num2} = {operation_1st_result}")
+        saved_result = operation_1st_result #switch404fix
+        #XXXXXXXXXXXXX
+        continue_calculations = input("do you want to continue your calculations? or start a new one? yes or no, chose [y/n]:").lower()
+
+        if continue_calculations == "yes" or continue_calculations == "y":
+            run_code_1st = False
+        elif continue_calculations == "no" or continue_calculations == "n":
+            saved_result = 0
+            reset()
+        else:
+            print("<!>INVALID choice, calculator will reset, you can start-over now")
+            saved_result = 0
+            reset()
+
+
+
+
+    #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    while run_code_1st == False and saved_result > 0:
+        result = saved_result
+        #------------
+        num1 = result
+        operation_choice = input("select a mathematical operation,\nfor addition [+]\nfor subtraction [-]\nfor multiplication [*]\nfor division [/]\nthe operation you chose is: ")
+        num2 = float(input("what's the next number?"))
+
+        #-------------------
+        if num1 in allowed_numbers == False or num2 in allowed_numbers == False:
+            print("<!>INVALID numbers")
+        if operation_choice in allowed_symbols == False:
+            print("<!>INVALID operation input, please only select from the following symbols [-][+][*][/]")
+
+        # ------------------
+        for operation_ver in allowed_symbols:
+            if operation_choice == operation_ver:
+                result = operations[operation_ver](num1,num2)
+        #------------------
+        print(f"result: {num1} {operation_choice} {num2} =  {result}")
+        #XXXXXXXXXXXXX
+        saved_result = result #switch404fix
+        continue_calculations = input("do you want to continue your calculations? or start a new one? yes or no, chose [y/n]:").lower()
+
+        if continue_calculations == "yes" or continue_calculations == "y":
+            run_code_1st = False
+        elif continue_calculations == "no" or continue_calculations == "n":
+            saved_result = 0
+            reset()
+        else:
+            print("<!>INVALID choice, calculator will reset, you can start-over now")
+            saved_result = 0
+            reset()
